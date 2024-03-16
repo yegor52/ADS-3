0a8613fa454be4ca27ac9edadba0b473d968b316
yegor52 <yegorbeat2005@gmail.com>
2024-03-16 13:33:14 +0300

    Sharov ADS-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,32 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int left = 0;
+    int right = size - 1;
+    int count = 0;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            count++;
+            int i = mid - 1;
+            while (arr[i] == value) {
+                count++;
+                i--;
+            }
+            int j = mid + 1;
+            while (arr[j] == value) {
+                count++;
+                j++;
+            }
+            return count;
+        }
+        else if (arr[mid] < value) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,5 @@
 int main() {
     int arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5};
     int count = cbinsearch(arr, 15, 3); // count = 3
-    return 0;
+    return count;
 }
